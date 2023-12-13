3f789ac714170dae468e2fabcae74e421fb394d2
davemanunee <dave.man@northeastern.edu>
2023-12-13 11:48:55 -0500

    Added, collection View, linking left between segues

---
+++ Harmony/Harmony/Models/Resources.swift
@@ -0,0 +1,22 @@
+//
+//  Resources.swift
+//  Harmony
+//
+//  Created by Manunee Dave on 12/13/23.
+//
+
+import UIKit
+
+public struct Resources {
+    let title: String
+    let image: UIImage
+}
+
+public let resources: [Resources] = [
+    Resources(title: "This is Video 1", image: #imageLiteral(resourceName: "harmony-logo")),
+    Resources(title: "This is Video 2", image: #imageLiteral(resourceName: "harmony-logo")),
+    Resources(title: "This is Video 3", image: #imageLiteral(resourceName: "harmony-logo")),
+    Resources(title: "This is Video 4", image: #imageLiteral(resourceName: "harmony-logo")),
+    Resources(title: "This is Video 5", image: #imageLiteral(resourceName: "harmony-logo"))
+
+]
